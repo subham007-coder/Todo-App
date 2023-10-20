cafe43ccac31f48db0efede7ad611f1994d87144
subham007-coder <dsubham109@gmail.com>
2023-10-20 20:27:18 +0530

    add

--- script.js
+++ script.js
@@ -3,19 +3,32 @@ const input = document.querySelector("input");
 const button = document.querySelector("button");
 const ul = document.querySelector("ul");
 const li = document.querySelectorAll("li");
+const dlt = document.querySelectorAll(".dlt");
+const makeLi = document.createElement("li");
 
 // p.textContent = "Task Add Successful..."
 
 button.addEventListener("click", () => {
     button.style.backgroundColor = "lightgreen"
-    const makeLi = document.createElement("li");
-    ul.append(makeLi);
+    ul.appendChild(makeLi);
     const makeString = String(makeLi.value)
     makeLi.innerHTML = input.value;
-})
+    input.value = "";
 
-const allLi = li.forEach( (e) => {
-    e.addEventListener("click", () => {
-        e.remove()
+    const makeBtn = document.createElement("button");
+    makeBtn.textContent = "Delete Task"
+    makeBtn.classList.add("dlt")
+
+    makeLi.appendChild(makeBtn);
+})
+const inpage = li.forEach((li) => {
+    dlt.forEach((allBtn) => {
+        // console.log(allBtn);
+        allBtn.addEventListener("click", () => {
+            console.log("remove");
+            li.remove()
+            makeLi.remove()
+        })
     })
-} )
+
+})
