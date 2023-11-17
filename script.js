d9d8c29f66fcb932e6fbc5d5f1e3ac0937a57776
subham007-coder <dsubham109@gmail.com>
2023-11-17 20:43:24 +0530

    add

--- script.js
+++ script.js
@@ -12,34 +12,27 @@ button.addEventListener("click", () => {
     if (input.value == "") {
         p.textContent = "Please Add A Task...";
         p.style.color = "red";
-        console.log("no");
         button.style.backgroundColor = "red";
         button.style.color = "white";
     } else {
         p.textContent = "Task Add Successful...";
         p.style.color = "lightgreen";
-        button.style.backgroundColor = "lightgreen"
-        button.style.color = "black"
+        button.style.backgroundColor = "lightgreen";
+        button.style.color = "black";
         ul.appendChild(makeLi);
         const makeString = String(makeLi.value);
         makeLi.innerHTML = input.value;
-        input.value = "";
 
         const makeBtn = document.createElement("button");
         makeBtn.textContent = "Delete Task";
         makeBtn.classList.add("dlt");
         makeLi.appendChild(makeBtn);
-    }
-})
+    };
+});
 
 ul.addEventListener("click", (event) => {
-
     if (event.target.nodeName == "BUTTON") {
-        console.log("dlt");
         const listItem = event.target.parentElement;
-        console.log(listItem);
         listItem.remove();
     }
 });
-
-localStorage.setItem("name", "subham")
