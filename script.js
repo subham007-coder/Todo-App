9e19d911a5ee38536f1710962ad0a67c0180070b
subham007-coder <dsubham109@gmail.com>
2023-12-09 09:07:16 +0530

    add

--- script.js
+++ script.js
@@ -1,42 +1,60 @@
-const p = document.querySelector("p");
-const input = document.querySelector("input");
-const button = document.querySelector("button");
-const ul = document.querySelector("ul");
-const li = document.querySelectorAll("li");
-const dlt = document.querySelectorAll(".dlt");
-const makeLi = document.createElement("li");
-
+// const p = document.querySelector("p");
+// const input = document.querySelector("input");
+// const button = document.querySelector("button");
+// const ul = document.querySelector("ul");
+// const li = document.querySelectorAll("li");
+// const dlt = document.querySelectorAll(".dlt");
+// const makeLi = document.createElement("li");
 
 //(now we can write sql queries in our CLI)
 // step : 1 cd "C:\Program Files\MySQL\MySQL Server 8.0\bin"
 // step : 2 .\mysql.exe -u root -p
+const mysql = require('mysql2');
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'todoapp',
+    password: '#adsuforever24'
+  });
 
-button.addEventListener("click", () => {
+  try {
+    connection.query("show tables", (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    })
+  } catch (error) {
+    console.log(error);
+  }
 
-    if (input.value == "") {
-        p.textContent = "Please Add A Task...";
-        p.style.color = "red";
-        button.style.backgroundColor = "red";
-        button.style.color = "white";
-    } else {
-        p.textContent = "Task Add Successful...";
-        p.style.color = "lightgreen";
-        button.style.backgroundColor = "lightgreen";
-        button.style.color = "black";
-        ul.appendChild(makeLi);
-        const makeString = String(makeLi.value);
-        makeLi.innerHTML = input.value;
+// button.addEventListener("click", () => {
 
-        const makeBtn = document.createElement("button");
-        makeBtn.textContent = "Delete Task";
-        makeBtn.classList.add("dlt");
-        makeLi.appendChild(makeBtn);
-    };
-});
+//     if (input.value == "") {
+//         p.textContent = "Please Add A Task...";
+//         p.style.color = "red";
+//         button.style.backgroundColor = "red";
+//         button.style.color = "white";
+//     } else {
+//         p.textContent = "Task Add Successful...";
+//         p.style.color = "lightgreen";
+//         button.style.backgroundColor = "lightgreen";
+//         button.style.color = "black";
+//         ul.appendChild(makeLi);
+//         const makeString = String(makeLi.value);
+//         makeLi.innerHTML = input.value;
 
-ul.addEventListener("click", (event) => {
-    if (event.target.nodeName == "BUTTON") {
-        const listItem = event.target.parentElement;
-        listItem.remove();
-    }
-});
+//         const makeBtn = document.createElement("button");
+//         makeBtn.textContent = "Delete Task";
+//         makeBtn.classList.add("dlt");
+//         makeLi.appendChild(makeBtn);
+//     };
+// });
+
+// ul.addEventListener("click", (event) => {
+//     if (event.target.nodeName == "BUTTON") {
+//         const listItem = event.target.parentElement;
+//         listItem.remove();
+//     }
+// });
+
+
