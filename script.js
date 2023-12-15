2abc25acb4e1a9b005c2306537350fe0b363f6bc
subham007-coder <dsubham109@gmail.com>
2023-12-15 20:06:20 +0530

    add

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-const p = document.querySelector("p");
-const input = document.querySelector("input");
-const button = document.querySelector("button");
-const ul = document.querySelector("ul");
-const li = document.querySelectorAll("li");
-const dlt = document.querySelectorAll(".dlt");
-const makeLi = document.createElement("li");
+// const p = document.querySelector("p");
+// const input = document.querySelector("input");
+// const button = document.querySelector("button");
+// const ul = document.querySelector("ul");
+// const li = document.querySelectorAll("li");
+// const dlt = document.querySelectorAll(".dlt");
+// const makeLi = document.createElement("li");
 
 
-button.addEventListener("click", () => {
+// button.addEventListener("click", () => {
 
-    if (input.value == "") {
-        p.textContent = "Please Add A Task...";
-        p.style.color = "red";
-        button.style.backgroundColor = "red";
-        button.style.color = "white";
-    } else {
-        p.textContent = "Task Add Successful...";
-        p.style.color = "lightgreen";
-        button.style.backgroundColor = "lightgreen";
-        button.style.color = "black";
-        ul.appendChild(makeLi);
-        const makeString = String(makeLi.value);
-        makeLi.innerHTML = input.value;
+//     if (input.value == "") {
+//         p.textContent = "Please Add A Task...";
+//         p.style.color = "red";
+//         button.style.backgroundColor = "red";
+//         button.style.color = "white";
+//     } else {
+//         p.textContent = "Task Add Successful...";
+//         p.style.color = "lightgreen";
+//         button.style.backgroundColor = "lightgreen";
+//         button.style.color = "black";
+//         ul.appendChild(makeLi);
+//         const makeString = String(makeLi.value);
+//         makeLi.innerHTML = input.value;
 
-        const makeBtn = document.createElement("button");
-        makeBtn.textContent = "Delete Task";
-        makeBtn.classList.add("dlt");
-        makeLi.appendChild(makeBtn);
-    };
-});
+//         const makeBtn = document.createElement("button");
+//         makeBtn.textContent = "Delete Task";
+//         makeBtn.classList.add("dlt");
+//         makeLi.appendChild(makeBtn);
+//     };
+// });
 
-ul.addEventListener("click", (event) => {
-    if (event.target.nodeName == "BUTTON") {
-        const listItem = event.target.parentElement;
-        listItem.remove();
-    }
-});
+// ul.addEventListener("click", (event) => {
+//     if (event.target.nodeName == "BUTTON") {
+//         const listItem = event.target.parentElement;
+//         listItem.remove();
+//     }
+// });
+
+
