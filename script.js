e37b03168930c2dc1ff6200c6cd0103946307ab6
subham007-coder <dsubham109@gmail.com>
2023-12-17 12:32:46 +0530

    add

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 
