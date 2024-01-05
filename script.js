8787bf8ab9c763467ab2a7edc4465abb58d5b6bc
subham007-coder <dsubham109@gmail.com>
2024-01-05 19:51:50 +0530

    add

--- script.js
+++ script.js
@@ -1,32 +1,70 @@
-// const p = document.querySelector("p");
-// const input = document.querySelector("input");
-// const button = document.querySelector("button");
-// const ul = document.querySelector("ul");
-// const li = document.querySelectorAll("li");
-// const dlt = document.querySelectorAll(".dlt");
-// const makeLi = document.createElement("li");
+const express = require("express");
+const app = express();
+const path = require("path");
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
-//(now we can write sql queries in our CLI)
-// step : 1 cd "C:\Program Files\MySQL\MySQL Server 8.0\bin"
-// step : 2 .\mysql.exe -u root -p
-const mysql = require('mysql2');
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'todoapp',
-  password: '#adsuforever24'
+
+const todoSchema = new mongoose.Schema({
+    todoName: {
+        type: String,
+    },
+    time_now: {
+        type: Date,
+    }
+});
+const Todo = mongoose.model("Todo", todoSchema);
+
+// const todo1 = new Todo({
+//     todoName: "Next add todo by user",
+//     time_now: new Date(),
+// });
+
+// todo1.save()
+//     .then(console.log("todo was save"))
+//     .catch(err => console.log(err));
+
+main()
+    .then(res => { console.log("i am mongoose function call"); })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/todo');
+}
+
+app.get("/", async (req, res) => {
+    const todos = await Todo.find().maxTimeMS(30000);
+    res.render("index.ejs", { todos });
+})
+
+app.get("/todo/new", (req, res) => {
+    res.render("new.ejs");
 });
 
-let q = "insert into work (todothis) values (?)";
+app.post("/add", async (req, res) => {
+    let { newTodo } = req.body;
+    let addTodo = new Todo({
+        todoName: newTodo,
+        time_now: new Date(),
+    });
+    await addTodo.save()
+        .then(res => { console.log("todo was save") })
+        .catch(err => { console.log(err) });
+    res.redirect("/");
+});
 
+app.delete("/todo/:id", async (req, res) => {
+    let {id} = req.params;
+    let deletedChat = await Todo.findByIdAndDelete(id).maxTimeMS(30000);
+    console.log(deletedChat);
+    res.redirect("/");
+});
 
-try {
-  connection.query(q, (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  })
-} catch (error) {
-  console.log(error);
-}
-connection.end();
+app.listen("8080", () => {
+    console.log("res send");
+});
